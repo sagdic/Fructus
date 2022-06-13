b1d2e4d14bda746b6a8679543910843bb991f134
Tayfun Sağdıç <30289904+sagdic@users.noreply.github.com>
2022-06-13 11:48:25 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by tayfun on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
