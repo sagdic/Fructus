c7ede8a7ccb6443041730084aad9515b7d09a1bd
Tayfun Sağdıç <30289904+sagdic@users.noreply.github.com>
2022-06-14 16:45:42 +0300

    14 06 22 end commit

---
+++ Fructus/App/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by tayfun on 13.06.2022.
+//
+// 
+
+import SwiftUI
+
+struct ContentView: View {
+  // MARK: - PROPERTIES
+  
+  @State private var isShowingSettings: Bool = false
+  
+  var fruits: [Fruit] = fruitsData
+
+  // MARK: - BODY
+
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(fruits.shuffled()) { item in
+          NavigationLink(destination: FruitDetailView(fruit: item)) {
+            FruitRowView(fruit: item)
+              .padding(.vertical, 4)
+          }
+        }
+      }
+      .navigationTitle("Fruits")
+      .navigationBarItems(
+        trailing:
+          Button(action: {
+            isShowingSettings = true
+          }) {
+            Image(systemName: "slider.horizontal.3")
+          } //: BUTTON
+          .sheet(isPresented: $isShowingSettings) {
+            SettingsView()
+          }
+      )
+    } //: NAVIGATION
+    .navigationViewStyle(StackNavigationViewStyle())
+  }
+}
+
+// MARK: - PREVIEW
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView(fruits: fruitsData)
+      .previewDevice("iPhone 11 Pro")
+  }
+}
